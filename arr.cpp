24e289017f033af74e63c0522ae20609d476814c
Polina Pyrlyk <Tsipileva123@mail.ru>
2020-12-20 20:03:20 +0300

    iii

---
+++ arr.cpp
@@ -0,0 +1,33 @@
+﻿// arr.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <ctime>
+
+
+
+using namespace std;
+
+
+void main()
+{
+	setlocale(LC_ALL, "rus");
+	srand(time(NULL));
+	int a = rand() % 10 + 5;
+	
+	if (a < 10)
+	{
+		cout << "слишком мало";
+
+	}
+	
+	else
+	{
+		cout << "ERROR";
+
+
+	}
+}
+
+
+
